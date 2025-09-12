7249d2f60711dc0f7f6d7cf60e8e5d65ec590bcc
stkisengese <skisengese@gmail.com>
2025-09-12 13:41:14 +0300

    feat: enhance logout functionality by adding session invalidation and token regeneration

--- app/Http/Controllers/Auth/LogoutController.php
+++ app/Http/Controllers/Auth/LogoutController.php
@@ -4,12 +4,15 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
 
 class LogoutController extends Controller
 {
-    public function logout()
+    public function logout(Request $request)
     {
         Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
 
         return redirect()->route('home');
     }
