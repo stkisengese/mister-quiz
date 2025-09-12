cc7f34a331cd39d9637017d39b39c31a364a8c81
stkisengese <skisengese@gmail.com>
2025-09-12 12:48:49 +0300

    feat: improve UX, accessibility and form validation

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,11 +9,23 @@
         <p class="title form-header">Login form</p>
     </div>
 
+    @if (session('status'))
+        <div class="center error-msg mt2">
+            {{ session('status') }}
+        </div>
+    @endif
+
     <form action="{{ route('login') }}" method="POST">
         @csrf
 
         <div class="mb4">
-            <input class="auth-input center" type="text" name="email" id="email" placeholder="Enter email" value="{{ old('email')}}">
+            <input class="auth-input center @error('email') error @enderror" 
+                   type="email" 
+                   name="email" 
+                   id="email" 
+                   placeholder="Enter email" 
+                   value="{{ old('email') }}"
+                   required>
 
             @error('email')
             <div class="center error-msg mt2">
@@ -23,7 +35,12 @@
         </div>
 
         <div class="mb4">
-            <input class="auth-input center" type="password" name="password" id="password" placeholder="Enter password">
+            <input class="auth-input center @error('password') error @enderror" 
+                   type="password" 
+                   name="password" 
+                   id="password" 
+                   placeholder="Enter password"
+                   required>
 
             @error('password')
             <div class="center error-msg mt2">
@@ -32,12 +49,15 @@
             @enderror
         </div>
 
-        <button class="center green-btn mb4" style="cursor: pointer;" type="submit">Login</button>
-        @if (session('status'))
-        <div class="center error-msg mt2">
-            {{ session('status') }}
+        <div class="mb4">
+            <label class="center checkbox-label">
+                <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}>
+                <span>Remember me</span>
+            </label>
         </div>
-        @endif
+
+        <button class="center green-btn mb4" style="cursor: pointer;" type="submit">Login</button>
+        
         <a class="center simple-link mt2" href="{{ route('register') }}">Don't have an account? Register</a>
     </form>
 </div>
