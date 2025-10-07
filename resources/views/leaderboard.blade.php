a4cb1ec0e793af582ce74a03aebb36c6666ce0e6
stkisengese <skisengese@gmail.com>
2025-10-08 00:56:58 +0300

    feat: Enhance leaderboard view with structured table and user data display

--- resources/views/leaderboard.blade.php
+++ resources/views/leaderboard.blade.php
@@ -2,8 +2,30 @@
 
 @section('content')
 
-<table>
+<a class="top-right-corner red-btn" href="{{ route('home') }}">Back ></a>
 
-</table>
+<div class="content">
+    <h2 class="title">Leaderboard</h2>
+    <table class="leaderboard-table">
+        <thead>
+            <tr>
+                <th>Rank</th>
+                <th>Username</th>
+                <th>XP</th>
+                <th>Total Correct</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($users as $index => $user)
+            <tr>
+                <td>{{ $index + 1 }}</td>
+                <td>{{ $user->username }}</td>
+                <td>{{ $user->xp }}</td>
+                <td>{{ $user->total_correct }}</td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
 
 @endsection
