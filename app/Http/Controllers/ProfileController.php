a50bca29c04d22a3f2fb61788fd8cfd7baca6f8d
stkisengese <skisengese@gmail.com>
2025-10-08 00:37:44 +0300

    refactor: Simplify user data retrieval and improve rank calculation in ProfileController

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -9,16 +9,30 @@ class ProfileController extends Controller
 {
     public function index()
     {
+        $user = Auth::user();
 
-        $user = Auth()->user();
+        // Calculate rank
+        $rank = match (true) {
+            $user->xp < 1500 => 'Quiz Apprentice',
+            $user->xp < 5000 => 'Average Quizer',
+            $user->xp < 10000 => 'Epic Quizer',
+            default => 'Quiz Master'
+        };
 
-        //variables that will be used to display user score in each category
-        $art = [];
-        $geography = [];
-        $history = [];
-        $science = [];
-        $sports = [];
+        // Parse category scores
+        $categories = ['art', 'geography', 'history', 'science', 'sports'];
+        $stats = [];
 
-        return view('profile', ['art' => $art, 'geography' => $geography, 'history' => $history, 'science' => $science, 'sports' => $sports]);
+        foreach ($categories as $category) {
+            [$correct, $total] = explode('/', $user->$category);
+            $percentage = $total > 0 ? round(($correct / $total) * 100, 1) : 0;
+            $stats[$category] = [
+                'correct' => $correct,
+                'total' => $total,
+                'percentage' => $percentage
+            ];
+        }
+
+        return view('profile', compact('user', 'rank', 'stats'));
     }
 }
