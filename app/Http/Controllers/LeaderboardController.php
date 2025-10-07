c2ae552bdce61e0093ef81d4d862e7a64f76b256
stkisengese <skisengese@gmail.com>
2025-10-08 00:56:31 +0300

    feat: Implement leaderboard functionality to display top users with total correct answers

--- app/Http/Controllers/LeaderboardController.php
+++ app/Http/Controllers/LeaderboardController.php
@@ -2,12 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class LeaderboardController extends Controller
 {
     public function index()
     {
-        return view('leaderboard');
+        $users = User::orderBy('xp', 'desc')
+            ->take(10)
+            ->get()
+            ->map(function ($user) {
+                $totalCorrect = 0;
+                $categories = ['art', 'geography', 'history', 'science', 'sports'];
+
+                foreach ($categories as $category) {
+                    [$correct] = explode('/', $user->$category);
+                    $totalCorrect += (int) $correct;
+                }
+
+                $user->total_correct = $totalCorrect;
+                return $user;
+            });
+
+        return view('leaderboard', compact('users'));
     }
 }
