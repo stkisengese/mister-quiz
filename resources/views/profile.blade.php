d28f97a8dfd0ca077b49d9e6fea5dd4c2d1cf86a
stkisengese <skisengese@gmail.com>
2025-10-08 00:36:46 +0300

    refactor: Update profile view to use user data directly instead of auth()

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -6,16 +6,36 @@
 
 <div style="margin-top:100px">
     <div class="profile-header">
-        <p class="title profile-name">{{ auth()->user()->username }}</p>
-        <p class="title profile-email">{{ auth()->user()->email }}</p>
+        <p class="title profile-name">{{ $user->username }}</p>
+        <p class="title profile-email">{{ $user->email }}</p>
     </div>
 
     <div class="profile-header">
-        <p class="title profile-xp">{{ auth()->user()->xp }} XP</p>
+        <p class="title profile-xp">{{ $user->xp }} XP</p>
+        <p class="title profile-rank">Rank: {{ $rank }}</p>
+    </div>
 
+    <div class="content">
+        <h2 class="title">Category Stats</h2>
+        <table class="leaderboard-table">
+            <thead>
+                <tr>
+                    <th>Category</th>
+                    <th>Correct/Total</th>
+                    <th>Percentage</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($stats as $category => $stat)
+                <tr>
+                    <td>{{ ucfirst($category) }}</td>
+                    <td>{{ $stat['correct'] }}/{{ $stat['total'] }}</td>
+                    <td>{{ $stat['percentage'] }}%</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
     </div>
 </div>
-
-
 
 @endsection
